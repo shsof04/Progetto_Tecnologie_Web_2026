efe2133f7e06b9e610302f4cfc383c815a387aab
Arianna <arianna.delmese@gmail.com>
2026-01-10 17:44:48 +0100

    db

--- db/creazione_db.sql
+++ db/creazione_db.sql
@@ -1,66 +1,63 @@
 CREATE DATABASE IF NOT EXISTS uniborankings
-  CHARACTER SET utf8mb4 -- cos'è
-  COLLATE utf8mb4_unicode_ci;
+    CHARACTER SET utf8mb4 -- cos'è
+    COLLATE utf8mb4_unicode_ci;
 
 USE uniborankings;
 
 -- Utenti (studenti + admin)
 CREATE TABLE IF NOT EXISTS utente (
-  id INT AUTO_INCREMENT PRIMARY KEY, -- no
-  nome VARCHAR(100) NOT NULL,
-  email VARCHAR(255) NOT NULL UNIQUE,
-  password VARCHAR(255) NOT NULL,
-  ruolo ENUM('STUDENTE','ADMIN') NOT NULL DEFAULT 'STUDENTE',
-  attivo TINYINT NOT NULL DEFAULT 1 -- ?
+    nome VARCHAR(100) NOT NULL,
+    utente_id VARCHAR(255) NOT NULL PRIMARY KEY,
+    password VARCHAR(255) NOT NULL,
+    ruolo ENUM('STUDENTE','ADMIN') NOT NULL DEFAULT 'STUDENTE',
+    attivo TINYINT NOT NULL DEFAULT 1,
+    immagineprofilo VARCHAR(255) NOT NULL
 ) ENGINE=InnoDB;
 
 -- Professori
 CREATE TABLE IF NOT EXISTS professore (
-  id INT AUTO_INCREMENT PRIMARY KEY, -- no
-  nome VARCHAR(120) NOT NULL,
-  email VARCHAR(255) NULL
+    nome VARCHAR(120) NOT NULL,
+    professore_id VARCHAR(255) NOT NULL PRIMARY KEY
 ) ENGINE=InnoDB;
 
 -- Corsi
 CREATE TABLE IF NOT EXISTS corso (
-  id INT AUTO_INCREMENT PRIMARY KEY, -- no
-  nome VARCHAR(140) NOT NULL,
-  cfu TINYINT NULL -- no?
+    corso_id VARCHAR(140) NOT NULL PRIMARY KEY
 ) ENGINE=InnoDB;
 
 -- Insegnamenti (chi tiene cosa e con che ruolo: lezioni/lab)
 CREATE TABLE IF NOT EXISTS insegnamento ( -- da cambiare in base alle chiavi giuste
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  professore_id INT NOT NULL,
-  corso_id INT NOT NULL,
-  ruolo ENUM('LEZIONI','LAB') NOT NULL DEFAULT 'LEZIONI',
-  anno_accademico VARCHAR(9) NOT NULL, -- es: 2025-2026
-  CONSTRAINT fk_ins_prof FOREIGN KEY (professore_id) REFERENCES professore(id)
-    ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT fk_ins_corso FOREIGN KEY (corso_id) REFERENCES corso(id)
-    ON DELETE CASCADE ON UPDATE CASCADE,
-  UNIQUE (professore_id, corso_id, ruolo, anno_accademico)
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    professore_id INT NOT NULL,
+    corso_id INT NOT NULL,
+    ruolo ENUM('LEZIONI','LAB') NOT NULL DEFAULT 'LEZIONI',
+    anno_accademico VARCHAR(9) NOT NULL, -- es: 2025-2026
+    CONSTRAINT fk_ins_prof FOREIGN KEY (professore_id) REFERENCES professore(id)
+      ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT fk_ins_corso FOREIGN KEY (corso_id) REFERENCES corso(id)
+      ON DELETE CASCADE ON UPDATE CASCADE,
+    UNIQUE (professore_id, corso_id, ruolo, anno_accademico)
 ) ENGINE=InnoDB;
 
 -- Recensioni
 CREATE TABLE IF NOT EXISTS recensione ( -- idem
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  utente_id INT NOT NULL,       -- chi scrive
-  professore_id INT NOT NULL,   -- prof recensito
-  corso_id INT NOT NULL,        -- corso
-  anno_accademico VARCHAR(9) NOT NULL,
-  voto_recensione TINYINT NOT NULL,      -- 1..10
-  voto_esame DECIMAL(4,1) NULL,          -- es: 28.5
-  data_appello DATE NULL,
-  testo TEXT NOT NULL,
-  data_creazione TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  data_modifica TIMESTAMP NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
-  CONSTRAINT fk_rec_utente FOREIGN KEY (utente_id) REFERENCES utente(id)
-    ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT fk_rec_prof FOREIGN KEY (professore_id) REFERENCES professore(id)
-    ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT fk_rec_corso FOREIGN KEY (corso_id) REFERENCES corso(id)
-    ON DELETE CASCADE ON UPDATE CASCADE,
-    
-    UNIQUE (utente_id, professore_id, corso_id, anno_accademico)
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    utente_id INT NOT NULL,       -- chi scrive
+    professore_id INT NOT NULL,   -- prof recensito
+    corso_id INT NOT NULL,        -- corso
+    anno_accademico VARCHAR(9) NOT NULL,
+    voto_recensione TINYINT NOT NULL,      -- 1..10
+    voto_esame DECIMAL(4,1) NULL,          -- es: 28.5
+    data_appello DATE NULL,
+    testo TEXT NOT NULL,
+    data_creazione TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    data_modifica TIMESTAMP NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+    CONSTRAINT fk_rec_utente FOREIGN KEY (utente_id) REFERENCES utente(id)
+      ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT fk_rec_prof FOREIGN KEY (professore_id) REFERENCES professore(id)
+      ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT fk_rec_corso FOREIGN KEY (corso_id) REFERENCES corso(id)
+      ON DELETE CASCADE ON UPDATE CASCADE,
+      
+      UNIQUE (utente_id, professore_id, corso_id, anno_accademico)
 ) ENGINE=InnoDB;
--- db/inserisci_dati.sql
+++ db/inserisci_dati.sql
@@ -1,24 +1,28 @@
 USE uniborankings;
 
--- Utenti
-INSERT INTO utente (nome, email, password, ruolo, attivo) VALUES -- attivo cos'è + studenti: Moira Quartieri, Nico Todeschi, Matteo Nencioni
-('Admin', 'admin@uniborankings.it', 'admin', 'ADMIN', 1),        -- mettiamo loro 3 così scriviamo delle recensioni fatte da studenti diversi
-('Blasco Pulieri', 'blasco.pulieri@studio.unibo.it', 'pass2026', 'STUDENTE', 1);
+-- Utenti id = email
+INSERT INTO utente (nome, utente_id, password, ruolo, immagineprofilo) VALUES 
+('Admin', 'admin@uniborankings.it', 'admin', 'ADMIN', 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d0/Seal_of_the_University_of_Bologna.svg/1280px-Seal_of_the_University_of_Bologna.svg.png'),
+('Moira Quartieri', 'moira.quartieri@studio.unibo.it', 'pass2026', 'STUDENTE', 'https://cinemaserietv.it/wp-content/uploads/2025/05/moira-quartieri.jpg'),
+('Nico Tedeschi', 'nico.tedeschi@studio.unibo.it', 'pass2026', 'STUDENTE', 'https://www.dueesseimmobiliare.com/wp-content/uploads/nico-tedeschi-immagine.png'),
+('Matteo Nencioni', 'matteo.nencioni@studio.unibo.it', 'pass2026', 'STUDENTE', 'https://blog.immobiliarenencioni.it/wp-content/uploads/2022/11/bc14d32c-fefc-4288-9442-57e662f62393.jpg');
 
--- Professori
-INSERT INTO professore (nome, email) VALUES -- AGGIUNGERE LE IMMAGINI QUI???
-('Gianluca Torre', 'gianluca.torre@unibo.it'),
-('Ida Di Filippo', 'ida.difilippo@unibo.it'), -- mettiamo come studenti quelli toscani quindi Blasco viene promosso
-('Mariana D''Amico', 'mariana.damico@unibo.it'),
-('Nadia Mayer', 'nadia.mayer@unibo.com'),
-('Corrado Sassu', 'corrado.sassu@unibo.com'),
-('Blasco Pulieri', 'blasco.pulieru@unibo.com');
+-- Professori id = email
+INSERT INTO professore (nome, professore_id, immagineprofilo) VALUES
+('Gianluca Torre', 'gianluca.torre@unibo.it', 'https://wips.plug.it/cips/libero.it/magazine/cms/2024/07/gianluca-torre.jpg?w=785&h=494&a=c'),
+('Ida Di Filippo', 'ida.difilippo@unibo.it', 'https://www.realizenetworks.com/wp-content/uploads/2023/07/ida-di-filippo.png'),
+('Mariana D''Amico', 'mariana.damico@unibo.it', 'https://wips.plug.it/cips/libero.it/magazine/cms/2024/11/mariana_damico.jpg'),
+('Nadia Mayer', 'nadia.mayer@unibo.com', 'https://media-assets.vanityfair.it/photos/68383323161cc8c418c7300d/4:3/w_1064,h_798,c_limit/Nadia%2019.jpeg'),
+('Corrado Sassu', 'corrado.sassu@unibo.com', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTr5hpmM4yLKOXBzjddzmNveH8-jjynGJibVw&s'),
+('Blasco Pulieri', 'blasco.pulieru@unibo.com', 'https://www.ritagiorgi.it/wp-content/uploads/2019/12/Blasco-Pulieri-400x500_c.jpg');
 
 -- Corsi
-INSERT INTO corso (nome, cfu) VALUES -- leviamo cfu? e mettiamo altri corsi
-('Architetture degli Elaboratori', 12),
-('Basi di Dati', 12),
-('Programmazione', 12);
+INSERT INTO corso (corso_id) VALUES
+('Architetture degli Elaboratori'),
+('Basi di Dati'),
+('Programmazione'),
+('Crittografia')
+('Tecnologie Web');
 
 -- Insegnamenti (lezioni)
 INSERT INTO insegnamento (professore_id, corso_id, ruolo, anno_accademico) VALUES -- come si specifica quale è l'id?
