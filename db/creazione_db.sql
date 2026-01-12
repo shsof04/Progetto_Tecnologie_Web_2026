83571d16decbbd4e2b1dfa5a5330ce28b6143323
Sofia <shensofia04@gmail.com>
2026-01-12 18:14:10 +0100

    Update project file

--- db/creazione_db.sql
+++ db/creazione_db.sql
@@ -7,59 +7,94 @@ USE uniborankings;
 -- Utenti (studenti + admin)
 CREATE TABLE IF NOT EXISTS utente (
     nome VARCHAR(100) NOT NULL,
-    utente_id VARCHAR(255) NOT NULL PRIMARY KEY,
+    utente_id VARCHAR(100) NOT NULL PRIMARY KEY,
     password VARCHAR(255) NOT NULL,
     ruolo ENUM('STUDENTE','ADMIN') NOT NULL DEFAULT 'STUDENTE',
     attivo TINYINT NOT NULL DEFAULT 1,
-    immagineprofilo VARCHAR(255) NOT NULL
+    immagineprofilo VARCHAR(255) NULL
 ) ENGINE=InnoDB;
 
 -- Professori
 CREATE TABLE IF NOT EXISTS professore (
-    nome VARCHAR(120) NOT NULL,
-    professore_id VARCHAR(255) NOT NULL PRIMARY KEY
+    nome VARCHAR(100) NOT NULL,
+    professore_id VARCHAR(100) NOT NULL PRIMARY KEY,
+    immagineprofilo VARCHAR(255) NULL
 ) ENGINE=InnoDB;
 
 -- Corsi
 CREATE TABLE IF NOT EXISTS corso (
-    corso_id VARCHAR(140) NOT NULL PRIMARY KEY
+    corso_id VARCHAR(100) NOT NULL PRIMARY KEY, 
+    nome VARCHAR(100) NOT NULL
 ) ENGINE=InnoDB;
 
 -- Insegnamenti (se il professore viene cancellato, vengono cancellati tutti i suoi esami)
 CREATE TABLE IF NOT EXISTS insegnamento (
-    professore_id VARCHAR(255) NOT NULL,
-    corso_id VARCHAR(140) NOT NULL,
+    professore_id VARCHAR(100) NOT NULL,
+    corso_id VARCHAR(100) NOT NULL,
     ruolo ENUM('LEZIONI','LABORATORIO') NOT NULL DEFAULT 'LEZIONI',
     anno_accademico VARCHAR(9) NOT NULL,
+
     PRIMARY KEY (professore_id, corso_id, ruolo, anno_accademico),
+
     CONSTRAINT fk_ins_prof FOREIGN KEY (professore_id)
         REFERENCES professore(professore_id)
         ON DELETE CASCADE ON UPDATE CASCADE,
+
     CONSTRAINT fk_ins_corso FOREIGN KEY (corso_id)
         REFERENCES corso(corso_id)
         ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB;
 
+-- Appelli
+CREATE TABLE IF NOT EXISTS appello (
+  professore_id VARCHAR(100) NOT NULL,
+  corso_id VARCHAR(100) NOT NULL,
+  anno_accademico VARCHAR(9) NOT NULL,
+  data_appello DATE NOT NULL,
+
+  PRIMARY KEY (professore_id, corso_id, anno_accademico, data_appello),
+
+  CONSTRAINT fk_app_prof FOREIGN KEY (professore_id)
+    REFERENCES professore(professore_id)
+    ON DELETE CASCADE ON UPDATE CASCADE,
+
+  CONSTRAINT fk_app_corso FOREIGN KEY (corso_id)
+    REFERENCES corso(corso_id)
+    ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB;
 
 -- Recensioni
 CREATE TABLE IF NOT EXISTS recensione (
-    utente_id INT NOT NULL,
-    professore_id INT NOT NULL,
-    corso_id INT NOT NULL,
+
+    utente_id VARCHAR(100) NOT NULL,
+    professore_id VARCHAR(100) NOT NULL,
+    corso_id VARCHAR(100) NOT NULL,
+
     anno_accademico VARCHAR(9) NOT NULL,
     voto_recensione TINYINT NOT NULL,
-    voto_esame DECIMAL(4,1) NULL,
-    data_appello DATE NULL,
+    voto_esame INT NOT NULL,
+    data_appello DATE NOT NULL,
     testo TEXT NOT NULL,
+
     data_creazione TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     data_modifica TIMESTAMP NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
-    PRIMARY KEY (professore_id, corso_id, anno_accademico, data_appello),
-    CONSTRAINT fk_rec_utente FOREIGN KEY (utente_id) REFERENCES utente(utente_id)
-      ON DELETE CASCADE ON UPDATE CASCADE,
-    CONSTRAINT fk_rec_prof FOREIGN KEY (professore_id) REFERENCES professore(professore_id)
-      ON DELETE CASCADE ON UPDATE CASCADE,
-    CONSTRAINT fk_rec_corso FOREIGN KEY (corso_id) REFERENCES corso(corso_id)
-      ON DELETE CASCADE ON UPDATE CASCADE,
-      
-      UNIQUE (utente_id, professore_id, corso_id, anno_accademico, data_appello)
+
+    PRIMARY KEY (utente_id, professore_id, corso_id, anno_accademico, data_appello),
+
+    CONSTRAINT fk_rec_utente FOREIGN KEY (utente_id) 
+    REFERENCES utente(utente_id)
+        ON DELETE CASCADE ON UPDATE CASCADE,
+
+    CONSTRAINT fk_rec_prof FOREIGN KEY (professore_id) 
+    REFERENCES professore(professore_id)
+        ON DELETE CASCADE ON UPDATE CASCADE,
+
+    CONSTRAINT fk_rec_corso FOREIGN KEY (corso_id) 
+    REFERENCES corso(corso_id)
+        ON DELETE CASCADE ON UPDATE CASCADE,
+
+    CONSTRAINT fk_rec_app FOREIGN KEY (professore_id, corso_id, anno_accademico, data_appello)
+    REFERENCES appello(professore_id, corso_id, anno_accademico, data_appello)
+        ON DELETE CASCADE ON UPDATE CASCADE
+
 ) ENGINE=InnoDB;
--- db/inserisci_dati.sql
+++ db/inserisci_dati.sql
@@ -2,64 +2,80 @@ USE uniborankings;
 
 -- Utenti id = email
 INSERT INTO utente (nome, utente_id, password, ruolo, immagineprofilo) VALUES 
-('Admin', 'admin@uniborankings.it', 'admin', 'ADMIN', 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d0/Seal_of_the_University_of_Bologna.svg/1280px-Seal_of_the_University_of_Bologna.svg.png'),
-('Moira Quartieri', 'moira.quartieri@studio.unibo.it', 'pass2026', 'STUDENTE', 'https://cinemaserietv.it/wp-content/uploads/2025/05/moira-quartieri.jpg'),
-('Nico Tedeschi', 'nico.tedeschi@studio.unibo.it', 'pass2026', 'STUDENTE', 'https://www.dueesseimmobiliare.com/wp-content/uploads/nico-tedeschi-immagine.png'),
-('Matteo Nencioni', 'matteo.nencioni@studio.unibo.it', 'pass2026', 'STUDENTE', 'https://blog.immobiliarenencioni.it/wp-content/uploads/2022/11/bc14d32c-fefc-4288-9442-57e662f62393.jpg');
+('Admin', 'admin@uniborankings.it', 'admin', 'ADMIN', 'logo.PNG'),
+('Moira Quartieri', 'moira.quartieri@studio.unibo.it', 'pass1', 'STUDENTE', 'MoiraQuartieri.jpeg'),
+('Nico Tedeschi', 'nico.tedeschi@studio.unibo.it', 'pass2', 'STUDENTE', 'NicoTedeschi.png'),
+('Matteo Nencioni', 'matteo.nencioni@studio.unibo.it', 'pass3', 'STUDENTE', 'MatteoNencioni.jpg');
 
 -- Professori id = email
 INSERT INTO professore (nome, professore_id, immagineprofilo) VALUES
-('Gianluca Torre', 'gianluca.torre@unibo.it', 'https://wips.plug.it/cips/libero.it/magazine/cms/2024/07/gianluca-torre.jpg?w=785&h=494&a=c'),
-('Ida Di Filippo', 'ida.difilippo@unibo.it', 'https://www.realizenetworks.com/wp-content/uploads/2023/07/ida-di-filippo.png'),
-('Mariana D''Amico', 'mariana.damico@unibo.it', 'https://wips.plug.it/cips/libero.it/magazine/cms/2024/11/mariana_damico.jpg'),
-('Nadia Mayer', 'nadia.mayer@unibo.com', 'https://media-assets.vanityfair.it/photos/68383323161cc8c418c7300d/4:3/w_1064,h_798,c_limit/Nadia%2019.jpeg'),
-('Corrado Sassu', 'corrado.sassu@unibo.com', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTr5hpmM4yLKOXBzjddzmNveH8-jjynGJibVw&s'),
-('Blasco Pulieri', 'blasco.pulieru@unibo.com', 'https://www.ritagiorgi.it/wp-content/uploads/2019/12/Blasco-Pulieri-400x500_c.jpg');
+('Gianluca Torre', 'gianluca.torre@unibo.it', 'GianlucaTorre.jpg'),
+('Ida Di Filippo', 'ida.difilippo@unibo.it', 'IdaDiFilippo.png'),
+('Mariana DAmico', 'mariana.damico@unibo.it', 'MarianaDAmico.jpg'),
+('Nadia Mayer', 'nadia.mayer@unibo.com', 'NadiaMayer.jpeg'),
+('Corrado Sassu', 'corrado.sassu@unibo.com', 'CorradoSassu.png'),
+('Blasco Pulieri', 'blasco.pulieru@unibo.com', 'BlascoPulieri.jpg');
 
 -- Corsi
-INSERT INTO corso (corso_id) VALUES
-('Architetture degli Elaboratori'),
-('Basi di Dati'),
-('Programmazione'),
-('Crittografia')
-('Tecnologie Web');
+INSERT INTO corso (corso_id, nome) VALUES
+('ArchEla', 'Architetture degli Elaboratori'),
+('BasiDati', 'Basi di Dati'),
+('Progr', 'Programmazione'),
+('Critt', 'Crittografia'),
+('TechWeb', 'Tecnologie Web');
 
 -- Insegnamenti (lezioni)
 INSERT INTO insegnamento (professore_id, corso_id, ruolo, anno_accademico) VALUES
-('gianluca.torre@unibo.it', 'Architetture degli Elaboratori', 'LEZIONI', '2025-2026'),
-('gianluca.torre@unibo.it', 'Architetture degli Elaboratori', 'LABORATORIO', '2025-2026'),
-('ida.difilippo@unibo.it', 'Basi di Dati', 'LEZIONI', '2025-2026'),
-('ida.difilippo@unibo.it', 'Basi di Dati', 'LABORATORIO', '2025-2026'),
-('mariana.damico@unibo.it', 'Programmazione', 'LEZIONI', '2025-2026'),
-('corrado.sassu@unibo.com', 'Programmazione', 'LABORATORIO', '2025-2026'),
-('nadia.mayer@unibo.com', 'Crittografia', 'LEZIONI', '2025-2026'),
-('nadia.mayer@unibo.com', 'Tecnologie Web', 'LEZIONI', '2025-2026'),
-('blasco.pulieru@unibo.com', 'Tecnologie Web', 'LABORATORIO', '2025-2026'),
+('gianluca.torre@unibo.it', 'ArchEla', 'LEZIONI', '2025-2026'),
+('gianluca.torre@unibo.it', 'ArchEla', 'LABORATORIO', '2025-2026'),
+('ida.difilippo@unibo.it', 'BasiDati', 'LEZIONI', '2025-2026'),
+('ida.difilippo@unibo.it', 'BasiDati', 'LABORATORIO', '2025-2026'),
+('mariana.damico@unibo.it', 'Progr', 'LEZIONI', '2025-2026'),
+('corrado.sassu@unibo.com', 'Progr', 'LABORATORIO', '2025-2026'),
+('nadia.mayer@unibo.com', 'Critt', 'LEZIONI', '2025-2026'),
+('nadia.mayer@unibo.com', 'TechWeb', 'LEZIONI', '2025-2026'),
+('blasco.pulieru@unibo.com', 'TechWeb', 'LABORATORIO', '2025-2026'),
 
-('blasco.pulieru@unibo.com', 'Architetture degli Elaboratori', 'LEZIONI', '2024-2025'),
-('gianluca.torre@unibo.it', 'Architetture degli Elaboratori', 'LABORATORIO', '2024-2025'),
-('ida.difilippo@unibo.it', 'Basi di Dati', 'LEZIONI', '2024-2025'),
-('ida.difilippo@unibo.it', 'Basi di Dati', 'LABORATORIO', '2024-2025'),
-('nadia.mayer@unibo.com', 'Programmazione', 'LEZIONI', '2024-2025'),
-('corrado.sassu@unibo.com', 'Programmazione', 'LABORATORIO', '2024-2025'),
-('mariana.damico@unibo.it', 'Crittografia', 'LEZIONI', '2024-2025'),
-('nadia.mayer@unibo.com', 'Tecnologie Web', 'LEZIONI', '2024-2025'),
-('corrado.sassu@unibo.com', 'Tecnologie Web', 'LABORATORIO', '2024-2025'),
+('blasco.pulieru@unibo.com', 'ArchEla', 'LEZIONI', '2024-2025'),
+('gianluca.torre@unibo.it', 'ArchEla', 'LABORATORIO', '2024-2025'),
+('ida.difilippo@unibo.it', 'BasiDati', 'LEZIONI', '2024-2025'),
+('ida.difilippo@unibo.it', 'BasiDati', 'LABORATORIO', '2024-2025'),
+('nadia.mayer@unibo.com', 'Progr', 'LEZIONI', '2024-2025'),
+('corrado.sassu@unibo.com', 'Progr', 'LABORATORIO', '2024-2025'),
+('mariana.damico@unibo.it', 'Critt', 'LEZIONI', '2024-2025'),
+('nadia.mayer@unibo.com', 'TechWeb', 'LEZIONI', '2024-2025'),
+('corrado.sassu@unibo.com', 'TechWeb', 'LABORATORIO', '2024-2025'),
 
-('gianluca.torre@unibo.it', 'Architetture degli Elaboratori', 'LEZIONI', '2023-2024'),
-('gianluca.torre@unibo.it', 'Architetture degli Elaboratori', 'LABORATORIO', '2023-2024'),
-('ida.difilippo@unibo.it', 'Basi di Dati', 'LEZIONI', '2023-2024'),
-('ida.difilippo@unibo.it', 'Basi di Dati', 'LABORATORIO', '2023-2024'),
-('mariana.damico@unibo.it', 'Programmazione', 'LEZIONI', '2023-2024'),
-('mariana.damico@unibo.it', 'Programmazione', 'LABORATORIO', '2023-2024'),
-('nadia.mayer@unibo.com', 'Crittografia', 'LEZIONI', '2023-2024'),
-('nadia.mayer@unibo.com', 'Tecnologie Web', 'LEZIONI', '2023-2024'),
-('blasco.pulieru@unibo.com', 'Tecnologie Web', 'LABORATORIO', '2023-2024');
+('gianluca.torre@unibo.it', 'ArchEla', 'LEZIONI', '2023-2024'),
+('gianluca.torre@unibo.it', 'ArchEla', 'LABORATORIO', '2023-2024'),
+('ida.difilippo@unibo.it', 'BasiDati', 'LEZIONI', '2023-2024'),
+('ida.difilippo@unibo.it', 'BasiDati', 'LABORATORIO', '2023-2024'),
+('mariana.damico@unibo.it', 'Progr', 'LEZIONI', '2023-2024'),
+('mariana.damico@unibo.it', 'Progr', 'LABORATORIO', '2023-2024'),
+('nadia.mayer@unibo.com', 'Critt', 'LEZIONI', '2023-2024'),
+('nadia.mayer@unibo.com', 'TechWeb', 'LEZIONI', '2023-2024'),
+('blasco.pulieru@unibo.com', 'TechWeb', 'LABORATORIO', '2023-2024');
 
--- Recensioni (utente_id = 2 è Blasco)
+-- Appelli
+INSERT INTO appello (professore_id, corso_id, anno_accademico, data_appello) VALUES
+('gianluca.torre@unibo.it', 'ArchEla',  '2025-2026', '2026-01-09'),
+('ida.difilippo@unibo.it',  'BasiDati', '2025-2026', '2026-01-15'),
+('mariana.damico@unibo.it', 'Progr',    '2025-2026', '2026-01-22'),
+
+('blasco.pulieru@unibo.com','ArchEla',  '2024-2025', '2025-09-14'),
+('nadia.mayer@unibo.com',   'TechWeb',  '2024-2025', '2025-08-29'),
+('corrado.sassu@unibo.com', 'TechWeb',  '2024-2025', '2024-12-23'),
+
+('gianluca.torre@unibo.it', 'ArchEla',  '2023-2024', '2024-01-13'),
+('ida.difilippo@unibo.it',  'BasiDati', '2023-2024', '2024-01-22'),
+('ida.difilippo@unibo.it',  'BasiDati', '2023-2024', '2024-02-18');
+
+
+
+-- Recensioni 
 INSERT INTO recensione
 (utente_id, professore_id, corso_id, anno_accademico, voto_recensione, voto_esame, data_appello, testo)
 VALUES
-('nico.tedeschi@studio.unibo.it', 'gianluca.torre@unibo.it', 'Architetture degli Elaboratori', '2025-2026', 9, 28, '2026-01-09', 'Spiega bene, esame coerente con le lezioni.'),
-('moira.quartieri@studio.unibo.it', 'ida.difilippo@unibo.it', 'Basi di Dati', '2025-2026', 8, 26, '2026-01-15', 'Corso chiaro, ma serve allenarsi molto sugli esercizi.'),
-('matteo.nencioni@studio.unibo.it', 'mariana.damico@unibo.it', 'Programmazione', '2025-2026', 8, 24, '2026-01-22', 'Ritmo veloce, ma spiegazioni utili e disponibili a ricevimento.');
+('nico.tedeschi@studio.unibo.it', 'gianluca.torre@unibo.it', 'ArchEla', '2025-2026', 9, 28, '2026-01-09', 'Spiega bene, esame coerente con le lezioni.'),
+('moira.quartieri@studio.unibo.it', 'ida.difilippo@unibo.it', 'BasiDati', '2025-2026', 8, 26, '2026-01-15', 'Corso chiaro, ma serve allenarsi molto sugli esercizi.'),
+('matteo.nencioni@studio.unibo.it', 'mariana.damico@unibo.it', 'Progr', '2025-2026', 8, 24, '2026-01-22', 'Ritmo veloce, ma spiegazioni utili e disponibili a ricevimento.');
