f251a9a773fb65a57db9d421b96455cdad4d50bb
Sofia <shensofia04@gmail.com>
2026-01-19 15:53:59 +0100

    Update project file

--- courses.php
+++ courses.php
@@ -3,7 +3,7 @@ require_once("bootstrap.php");
 requireLogin();
 
 $templateParams["titolo"] = "UniboRankings - Corsi";
-$templateParams["nome"] = "courses-content.php";
+$templateParams["nome"] = "pagina-corso.php";
 
 $templateParams["anno_accademico"] = CURRENT_AA;
 $templateParams["corsi"] = $dbh->getCoursesByAcademicYear(CURRENT_AA);
--- exams.php
+++ exams.php
@@ -3,7 +3,7 @@ require_once("bootstrap.php");
 requireLogin();
 
 $templateParams["titolo"] = "UniboRankings - Esami";
-$templateParams["nome"] = "template/exams-content.php";
+$templateParams["nome"] = "pagina-esame.php";
 
 $templateParams["anno_accademico"] = CURRENT_AA;
 $templateParams["esami"] = $dbh->getExamsByAcademicYear(CURRENT_AA);
